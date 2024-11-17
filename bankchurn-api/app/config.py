64913e9657e2c80f98e82ca1ac0c4e165bba7d17
calipso82 <kliz8@hotmail.com>
2024-11-17 15:31:16 -0500

    Update PROJECT_NAME with my name

--- bankchurn-api/app/config.py
+++ bankchurn-api/app/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Banckchurn API"
+    PROJECT_NAME: str = "BankChurnProject_CarinaOrdonez"
 
     class Config:
         case_sensitive = True
